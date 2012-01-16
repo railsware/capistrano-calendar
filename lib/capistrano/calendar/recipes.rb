9c640015839bc85a8d014b0b790f0a0f7d7eb4bb
Roman V. Babenko <romanvbabenko@gmail.com>
2012-01-16 09:26:54 +0200

    [^] Bundler support has been added

--- lib/capistrano/calendar/recipes.rb
+++ lib/capistrano/calendar/recipes.rb
@@ -9,13 +9,15 @@ Capistrano::Configuration.instance.load do
     }
   end
 
+  set :try_bundle, fetch(:bundle_cmd, nil) ? "#{ fetch(:bundle_cmd) } exec " : ''
+
   namespace :calendar do
     desc "Create calendar event"
     task :create_event do
       configuration = Capistrano::Calendar::Configuration.collect(self)
       string = Capistrano::Calendar::Configuration.encode(configuration)
 
-      run("capistrano-calendar _#{Capistrano::Calendar::VERSION}_ create_event #{string}", calendar_runner)
+      run("#{ try_bundle } capistrano-calendar _#{ Capistrano::Calendar::VERSION }_ create_event #{ string }", calendar_runner)
     end
 
   end
